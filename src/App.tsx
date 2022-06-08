b5057174d449b53f7a63493157d3bbd1bfc58dbb
Bozhena Kondrat <89923581+bkondrat@users.noreply.github.com>
2022-06-08 16:59:40 +0200

    title, project, experience containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { Layout } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
+import ExperienceContainer from './components/Experience/ExperienceContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
+import ProjectsContainer from './components/Projects/ProjectsContainer';
+import TitleContainer from './components/Title/TitleContainer';
 
 function App() {
 
   return (
   <Layout className="layout">
     <HeaderContainer />
+    <Content>
+      <TitleContainer />
+      <ProjectsContainer />
+      <ExperienceContainer />
+    </Content>
     {/* <Content style={{ padding: '0 50px' }}>
 
       <div className="site-layout-content">Content</div>
--- src/components/Title/TitleContainer.tsx
+++ src/components/Title/TitleContainer.tsx
@@ -1,17 +1,14 @@
-import { Layout } from 'antd';
 import "./TitleContainer.css";
-
-const { Header } = Layout;
 
 const TitleContainer = () => {
     return(
         <div className="section_about">
-		<div className="container">
-			<h2 className="Name">Nazar Omeliukh</h2>
-			<p className="Designer">Product Designer</p>
-			<h1 className="AboutMe">Hey there! I’m a creative web designer based in sunny Lviv, Ukraine.</h1>
-		</div>
-	</div>
+            <div className="container">
+                <h2 className="name">Nazar Omeliukh</h2>
+                <p className="designer">Product Designer</p>
+                <h1 className="about-me">Hey there! I'm a creative web designer based in sunny Lviv, Ukraine.</h1>
+            </div>
+        </div>
     );
 }
 
